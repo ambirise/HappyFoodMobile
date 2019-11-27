e03b22fe75b3ddceda5f51f59d448bd740555c74
unknown <dhakalbikram891@gmail.com>
2019-11-27 14:24:23 +0545

    new file

--- appsrc/loginandregister/login.js
+++ appsrc/loginandregister/login.js
@@ -6,9 +6,40 @@ export class login extends Component {
         return (
             <View>
                 <Text> textInComponent </Text>
+                {/* <Text>Hello</Text> */}
             </View>
         )
     }
 }
+// all the files have been disappear
 
 export default login
+
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+class MyClass extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>MyClass</Text>
+            </View>
+        );
+    }
+}
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2c3e50',
+    },
+});
+
+//make this component available to the app
+export default MyClass;
+
